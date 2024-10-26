68306995cf335677655c7ddbcde8efc284da8c16
Ivan Laushkin <laushkinivan1@gmail.com>
2024-10-26 17:32:03 +0200

    modified uloha6_doplnok.py

--- prog2024/cvicenie5/sekcia2/uloha6_doplnok.py
+++ prog2024/cvicenie5/sekcia2/uloha6_doplnok.py
@@ -1,11 +1,16 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from decimal import Decimal
+from dateutil.relativedelta import relativedelta
 
 def hanojske_veze_64() -> str:
-    days = (2**64) - 1
+    days = Decimal((2**64) - 1)
     now = datetime.now()
-    future_date = now + timedelta(days=days)
-    years_away = (future_date - now).days / 365.25
-    return f"To end of the world is {years_away} years away\nIt will happen on {str(future_date)}"
+    years_to_add = int(days/Decimal(365.25))
+    remaining_days = int(days % Decimal(365.25))
+
+    future_date = now + relativedelta(days=remaining_days)
+    years_away = years_to_add + (remaining_days / 365.25)
+    return f"To end of the world is {years_away} years away\nIt will happen on {int(future_date.strftime("%Y")) + years_to_add}-{future_date.strftime("%m-%d")}"
 
 if __name__ == "__main__":
     print(hanojske_veze_64())

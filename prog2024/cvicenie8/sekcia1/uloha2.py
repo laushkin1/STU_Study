0e54d10bf454f3d956ed7ecef90f2ffb6e7c1386
Ivan Laushkin <laushkinivan1@gmail.com>
2024-11-16 17:46:25 +0100

    add some files

--- prog2024/cvicenie8/sekcia1/uloha2.py
+++ prog2024/cvicenie8/sekcia1/uloha2.py
@@ -1,14 +1,4 @@
 
-#def je_palindrom(ret: str) -> bool:
-#    if len(ret)%2:
-#        if ret[:int((len(ret))/2)] == ret[int((len(ret)+1)/2):][::-1]:
-#            return True
-#    else:
-#        if ret[:int(len(ret)/2)] == ret[int(len(ret)/2):][::-1]:
-#            return True
-#    return False
-
-
 def je_palindrom(ret: str) -> bool:
     if ret == ret[::-1]: return True
     return False

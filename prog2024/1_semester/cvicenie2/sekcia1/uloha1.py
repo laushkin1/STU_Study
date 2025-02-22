f80e5dcbe26a6bab47331ef9f9f6d813636e2868
Ivan Laushkin <laushkinivan1@gmail.com>
2025-02-22 14:29:16 +0100

    deleted prog2024/1_semester/cvicenie2

--- prog2024/1_semester/cvicenie2/sekcia1/uloha1.py
+++
@@ -1,8 +0,0 @@
-
-def tretia_mocnina(a: int) -> int:
-    print(a*a*a)
-
-if __name__ == "__main__":
-    tretia_mocnina(2) # 8
-    tretia_mocnina(3) # 27
-
--- prog2024/1_semester/cvicenie2/sekcia1/uloha4.py
+++
@@ -1,8 +0,0 @@
-
-def priemer_troch(a:int, b:int, c:int) -> float:
-    return round(((a+b+c)/3), 2)
-
-if __name__ == "__main__":
-    print(priemer_troch(1, 2, 3)) # 2.0
-    print(priemer_troch(1, 2, 4.5)) # 2.5
-

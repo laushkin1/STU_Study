0575c2a44601c5848246a2bd8ab7ee1c42590164
Ivan Laushkin <laushkinivan1@gmail.com>
2025-02-22 14:29:56 +0100

    deleted prog2024/1_semester/cvicenie8

--- prog2024/1_semester/cvicenie8/sekcia1/uloha1.py
+++
@@ -1,9 +0,0 @@
-
-def pocet_vyskytov(retazec: str, podretazec: str) -> int:
-    return retazec.count(podretazec)
-
-if __name__ == "__main__":
-    print(pocet_vyskytov("banana", "a"))
-    print(pocet_vyskytov("banana", "an"))
-    print(pocet_vyskytov("banana", "ana"))
-

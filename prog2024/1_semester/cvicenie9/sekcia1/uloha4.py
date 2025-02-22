38a52f89179e81dfb0af8fcd1e68f675078d1274
Ivan Laushkin <laushkinivan1@gmail.com>
2025-02-22 14:30:01 +0100

    deleted prog2024/1_semester/cvicenie9

--- prog2024/1_semester/cvicenie9/sekcia1/uloha4.py
+++
@@ -1,8 +0,0 @@
-
-def sucet_parnych_pozicii(zoznam_cisel: list[int]) -> int:
-    return sum(zoznam_cisel[i] for i in range(len(zoznam_cisel)) if i%2==1)
-
-
-if __name__ == "__main__":
-    print(sucet_parnych_pozicii([2, 5, 3, -2, -3, 0, 1]))
-

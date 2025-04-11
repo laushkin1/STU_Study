53212455e23e28017b13da52e6aa6c3d8e2fcd68
laushkin1 <laushkinivan1@gmail.com>
2025-04-11 06:14:58 +0200

    feat(add): add zadanie4 core

---
+++ prog2025/Programing/zadanie4/src/z4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "data.h" // NEMENIT, inak vas kod neprejde testom !!!
+
+// chybove hlasenia
+#define ERR_UNIT_COUNT "ERR_UNIT_COUNT"
+#define ERR_ITEM_COUNT "ERR_ITEM_COUNT"
+#define ERR_WRONG_ITEM "ERR_WRONG_ITEM"
+#define ERR_SLOTS "ERR_SLOTS"
+
+int main(const int argc, char *argv[]) {
+    return 0;
+}

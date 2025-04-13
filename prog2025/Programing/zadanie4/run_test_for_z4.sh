9d206121078510128145e6c624d1e1d8d917cc29
laushkin1 <laushkinivan1@gmail.com>
2025-04-13 15:55:15 +0200

    test: make 100% testing

--- prog2025/Programing/zadanie4/run_test_for_z4.sh
+++ prog2025/Programing/zadanie4/run_test_for_z4.sh
@@ -16,7 +16,35 @@ for scenario in "$TEST_DIR"/stdin/scenar*/; do
             continue
         fi
 
-        $EXEC < "$infile" > temp_output.txt
+        if [[ "$scenario_name" == "scenar_2" ]]; then
+            $EXEC 0 < "$infile" > temp_output.txt
+        elif [[ "$scenario_name" == "scenar_4" ]]; then
+            if [[ "$testnum" == "1" ]]; then
+                $EXEC 10 < "$infile" > temp_output.txt
+            elif [[ "$testnum" == "2" ]]; then
+                $EXEC 5 < "$infile" > temp_output.txt
+            elif [[ "$testnum" == "3" ]]; then
+                $EXEC 14 < "$infile" > temp_output.txt
+            elif [[ "$testnum" == "4" ]]; then
+                $EXEC 12 < "$infile" > temp_output.txt
+            elif [[ "$testnum" == "5" ]]; then
+                $EXEC 11 < "$infile" > temp_output.txt
+            elif [[ "$testnum" == "6" ]]; then
+                $EXEC 16 < "$infile" > temp_output.txt
+            elif [[ "$testnum" == "7" ]]; then
+                $EXEC 10 < "$infile" > temp_output.txt
+            elif [[ "$testnum" == "8" ]]; then
+                $EXEC 12 < "$infile" > temp_output.txt
+            elif [[ "$testnum" == "9" ]]; then
+                $EXEC 190 < "$infile" > temp_output.txt
+            else
+              printf " ERROR in scenar_4, witch param should be in example_$testnum ?"
+            fi
+        else
+            $EXEC < "$infile" > temp_output.txt
+        fi
+
+
 
         if diff temp_output.txt "$expected_outfile" --ignore-trailing-space >/dev/null; then
             printf "   -- %-20s ✅ Done\n" "$testnum"

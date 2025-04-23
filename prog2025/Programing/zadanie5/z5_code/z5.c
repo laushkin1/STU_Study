a69d3a298e493075b720c9c6e1ed1d1803282de0
laushkin1 <laushkinivan1@gmail.com>
2025-04-23 11:21:21 +0200

    feat: add main files

---
+++ prog2025/Programing/zadanie5/z5_code/z5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// NEMENIT, inak vas kod neprejde testom !!!
+#include "data.h" 
+
+int main(const int argc, char *argv[]) {
+	return 0;
+}

24c8c4c1f3626fcecd41db0eb66c10b7cafc2d97
laushkin1 <laushkinivan1@gmail.com>
2025-04-24 12:03:14 +0200

    docs: modified run_test_for_z5.sh

--- prog2025/Programing/zadanie5/run_test_for_z5.sh
+++ prog2025/Programing/zadanie5/run_test_for_z5.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-EXEC="./z5_code/z5" # Path to the executable file
+EXEC="./z5_code/z5git " # Path to the executable file
 TEST_DIR="./z5_testing" # Path to tests dir
 JSON_TXT="json_txt" # Path to json_txt dir
+DEBAG_MODE=0; # Change to 1 to turn on debag mode
 
 for scenario in "$TEST_DIR"/cmd/s*/; do
     scenario_name=$(basename "$scenario")
@@ -14,39 +15,61 @@ for scenario in "$TEST_DIR"/cmd/s*/; do
         expected_outfile="$TEST_DIR/output/$scenario_name/example$testnum.txt"
 
 
-
         if [ ! -f "$instruction_file" ]; then
-            printf "   -- %-20s ❌ The expected instruction file is missing\n" "$instruction_file"
+            printf "   -- %-20s ❌ Missing instruction file\n" "$testnum"
             continue
         fi
-
         if [ ! -f "$expected_outfile" ]; then
-            printf "   -- %-20s ❌ The expected output file is missing\n" "$testnum"
+            printf "   -- %-20s ❌ Missing expected output\n" "$testnum"
             continue
         fi
 
 
-        command=$(grep "^Spustenie:" "$instruction_file" | sed 's/Spustenie: //' | sed 's/z5\.exe//') # Remuve .exe
+        command=$(grep "^Spustenie:" "$instruction_file" \
+                | sed 's/Spustenie: //' | sed 's/z5\.exe//') # Remuve .exe
         command=$(echo "$command" | sed "s#txt/#$JSON_TXT/txt/#g")
 
-        full_command="$EXEC ${command# }"
+        full_command=($EXEC $command)
 
 
-        json_file=$(grep "^Databaza predmetov:" "$instruction_file" | sed 's/Databaza predmetov: //' | sed 's/ (.*)//')
+        json_file=$(grep "^Databaza predmetov:" "$instruction_file" \
+                    | sed 's/Databaza predmetov: //' | sed 's/ .*//')
         if [[ "$json_file" =~ \.json$ ]]; then
-            path_to_json_file="$JSON_TXT/json/$json_file"
-            cp "$path_to_json_file" "$(dirname "$EXEC")/items.json"
+            cp "$JSON_TXT/json/$json_file" "./items.json"
         fi
 
-        $full_command > temp_output.txt
+        # $full_command > temp_output.txt
 
-        if diff temp_output.txt "$expected_outfile" --ignore-trailing-space >/dev/null; then
-            printf "   -- %-20s ✅ Done\n" "$testnum"
+        if [[ "$scenario_name" == "s1" || "$scenario_name" == "s5" ]]; then
+            "${full_command[@]}" > temp_output.txt 2>/dev/null
         else
-            printf "   -- %-20s ❌ False\n" "example_$testnum"
+            "${full_command[@]}" > /dev/null 2> temp_output.txt
+        fi
+        
+
+        if diff temp_output.txt "$expected_outfile" \
+            --ignore-trailing-space --strip-trailing-cr >/dev/null; then
+                printf "   -- %-20s ✅ Done\n" "$testnum"
+        else
+            
+            if (( DEBAG_MODE )); then
+                printf "   -- %-20s ❌ False\n" "example_$testnum"
+                echo "------------------------------"
+                printf "\e[31m"
+                printf "Your output:\n"
+                cat temp_output.txt
+                printf "\e[32m"
+                printf "Correct output:\n"
+                cat $expected_outfile
+                echo "------------------------------"
+                printf "\e[37m\n\n"
+            else
+                printf "   -- %-20s ❌ False\n" "example_$testnum"
+            fi
+            
         fi
     done
 done
 
-rm -f "$(dirname "$EXEC")/items.json"
+rm -f "./items.json"
 rm -f temp_output.txt
